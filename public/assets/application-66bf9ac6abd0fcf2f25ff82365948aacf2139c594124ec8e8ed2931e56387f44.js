2945ba5aa4e69a8aa9a03af9aabef1a3dd3f8b2f
Kristina Brown <kristina.frey.frey@gmail.com>
2015-08-11 17:15:27 -0600

    pc

--- public/assets/application-66bf9ac6abd0fcf2f25ff82365948aacf2139c594124ec8e8ed2931e56387f44.js
+++ public/assets/application-66bf9ac6abd0fcf2f25ff82365948aacf2139c594124ec8e8ed2931e56387f44.js
@@ -10843,13 +10843,32 @@ var $listsDiv = $(".lists");
   $.get("/lists").then(function(lists){
     renderedLists = lists.map(generateList);
     renderedLists.forEach(listClick);
+    renderedLists.forEach(listClickDelete);
+    $(".new-list").empty();
+    $listsDiv.empty();
    $listsDiv.append(renderedLists);
+   var form = listForm();
+   addNewListEvent(form);
+   $(".new-list").append(form);
+   $listsDiv.prepend("<a class='new-list-form'>Create New List</a>")
+   $(".new-list-form").on("click", function(){$(".list-form").toggleClass("hidden");})
  });
 }
 
+function listForm(){
+  return $("<form class='col s12 hidden list-form'><div class='row'> <div class='input-field col s6'>" +
+          "<input id='new-list-title' type='text' class='validate'> <label for='text'>Title</label>" +
+          "</div></div>" +
+          "<div class='row'><div class='input-field col s12'><textarea class='materialize-textarea' id='new-list-description'></textarea><label for='text'>Description</label>" +
+          "<div class='row'> <div class='input-field col s5'><button class='btn waves-effect waves-light' id='create-list' type='submit' name='action'>Create List</button>"+
+          "</div></div></form>");
+    
+}
+
 function generateList(list){
-  return $("<div class='row' data-id="+ list.id + "> <h4 class=''>" + list.title + "</h4>" +
-        "<a href='#' class='link center'>Click to view tasks</a></div>" );
+  return $("<div class='row'> <h4 class=''>" + list.title + "</h4></div>" +
+        "<div class = 'row' data-id="+ list.id + "><a href='#' class='link'>Click to view tasks</a>" + 
+        " | <a href='#' class='delete '>Delete This List</a></div>" );
 }
 
 function listClick(list){
@@ -10870,6 +10889,43 @@ function listClick(list){
   });
 }
 
+function listClickDelete(list){
+  $(list).find(".delete").on("click", function() {
+    var $list = $(this).parents(".row");
+    var id = $list.data("id");
+    var form = taskForm(id);
+    $.ajax({
+      method: "DELETE",
+      url: "/lists/" + id,
+      data: { id: id }, 
+      success:  function(){
+        getLists();
+      }
+    });
+  });
+}
+
+function addNewListEvent(form){
+  $(form).find("#create-list").on("click", function() {
+    var $form = $(this).parents(".list-form");
+    var title = $form.find("#new-list-title").val();
+    var note = $form.find("#new-list-description").val();
+    
+    $.post("/lists", { title: title, description: note } ).then(function(task){
+      $(".list-form").toggleClass("hidden")
+      getLists();
+    });
+  });
+}
+
+;
+(function() {
+
+
+}).call(this);
+$(document).ready(function() {
+  displayAll();
+});
 function addNewEvent(form){
   $(form).find("#create-task").on("click", function() {
     var $form = $(this).parents(".task-form");
@@ -10897,7 +10953,7 @@ function taskForm(id){
           "<div class='row'><div class='input-field col s12'><textarea class='materialize-textarea' id='note'></textarea><label for='text'>Note</label>" +
           "<div class='list_id hidden'>" + id + "</div>" +
           "<div class='row'<div class='file-field input-field'> <input type='file' id='attachment' /></div></div>" +
-          "<div class='row'> <div class='input-field col s5'><button class='btn waves-effect waves-light' id='create-task' type='submit' name='action'>Create</button>"+
+          "<div class='row'> <div class='input-field col s5'><button class='btn waves-effect waves-light' id='create-task' type='submit' name='action'>Create Task</button>"+
           "</div></div></form>");
     
 }
@@ -10912,13 +10968,6 @@ function generateTask(task){
          "<a class='delete right' href='#'>Delete</a> <a class='right' href='/tasks/" + task.id + "/edit'>Edit</a> </div> </div> </div> </div> </div>");
 }
 ;
-(function() {
-
-
-}).call(this);
-$(document).ready(function() {
-  displayAll();
-});
 (function() {
 
 
