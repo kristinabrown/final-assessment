ec0ea09e142dac77f5c7ea94ef64755073d28bef
Kristina Brown <kristina.frey.frey@gmail.com>
2015-08-11 06:47:33 -0600

    testing frameworks set up

---
+++ spec/javascripts/test_spec.js
@@ -0,0 +1,7 @@
+describe("testing it's connected", function() {
+  
+  it("returns true", function() {
+    assert.equal(5, 5);
+  });
+
+});
---
+++ spec/test_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe "is should be hooked up right" do
+  it "it should return true" do
+    expect(true).to eq(true)
+  end
+end
